0b8a8a00d66c7270efbb1551cfc61af1b2c079c1
Lucas Domingos <lucasdomingos07@gmail.com>
2025-12-30 16:15:45 +0000

    diminua o tamanho da imagem que está abaixo da barra

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,8 +31,8 @@ export default function UpsellPage() {
                 <Image 
                   src="https://i.imgur.com/EhUv9ej.png" 
                   alt="Chat finalizado" 
-                  width={400} 
-                  height={75} 
+                  width={300} 
+                  height={56} 
                   className="mx-auto" 
                 />
               </div>
